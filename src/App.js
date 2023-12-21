faba03efe3477ac4cd629289f7c9ea2cabe4ebbb
Brandon Preston <brandon.preston@gmail.com>
2023-12-21 17:08:44 -0500

    Checking in progress on thumbnails

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Header from './components/Header';
+import { itemImages, items } from './items';
+import Thumbnail from './components/Thumbnail';
 
 function App() {
   return (
     <div>
       <Header />
+      {
+        items.map((item) => (
+          <Thumbnail key={item.itemId} image={[itemImages[item.imageId]]} title={item.title} />
+        ))
+      }
     </div>
   );
 }
