15a3d427680e38d59ce50ffc1343137ae839d2d4
Brandon Preston <brandon.preston@gmail.com>
2024-01-02 17:39:11 -0500

    Added subtotal to cart.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,28 +4,40 @@ import './Cart.css';
 import CartRow from './CartRow';
 
 function Cart({ cart, items, dispatch }) {
+  const subTotal = cart.reduce((acc, item) => {
+    const detailItem = items.find((i) => i.itemId === item.itemId);
+    const itemPrice = detailItem.salePrice ?? detailItem.price;
+    return item.quantity * itemPrice + acc;
+  }, 0);
+
   return (
     <div className="cart-component">
       <h2>Your Cart</h2>
       {cart.length === 0
         ? <div>Your cart is empty.</div>
         : (
-          <table>
-            <thead>
-              <tr>
-                <th>Quantity</th>
-                <th>Item</th>
-                <th>Price</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
+          <>
+            <table>
+              <thead>
+                <tr>
+                  <th>Quantity</th>
+                  <th>Item</th>
+                  <th>Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
                 cart.map((item) => (
                   <CartRow key={item.itemId} cartItem={item} items={items} dispatch={dispatch} />
                 ))
               }
-            </tbody>
-          </table>
+              </tbody>
+            </table>
+            <div>
+              Subtotal: $
+              {subTotal.toFixed(2)}
+            </div>
+          </>
         )}
     </div>
   );
